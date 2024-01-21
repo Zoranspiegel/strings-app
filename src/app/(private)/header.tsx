807be76650b70c7ebc45bc92fbfdcbb025d06e46
Juan José Rodriguez <zoranbow@gmail.com>
2024-01-21 17:07:06 -0500

    swr fetcher

--- src/app/(private)/header.tsx
+++ src/app/(private)/header.tsx
@@ -1,4 +1,14 @@
+'use client';
+import useSWR from 'swr';
+
 export default function Header (): JSX.Element {
+  const { data, error, isLoading } = useSWR('/api/users/profile');
+
+  if (error) return <div>Failed to load</div>;
+  if (isLoading) return <div>Loading...</div>;
+
+  console.log(data);
+
   return (
     <header>
       HEADER
--- src/app/(private)/layout.tsx
+++ src/app/(private)/layout.tsx
@@ -1,16 +1,23 @@
+'use client';
 import Header from './header';
 import NavBar from './navbar';
 import Footer from './footer';
+import fetcher from '../util/fetcher';
+import { SWRConfig } from 'swr';
 
 export default function PrivateLayout ({
   children
 }: { children: React.ReactNode }): JSX.Element {
   return (
-    <div>
-      <Header />
-      <NavBar />
-      <main>{children}</main>
-      <Footer />
-    </div>
+    <SWRConfig
+      value={{ fetcher }}
+    >
+      <div>
+        <Header />
+        <NavBar />
+        <main>{children}</main>
+        <Footer />
+      </div>
+    </SWRConfig>
   );
 }
