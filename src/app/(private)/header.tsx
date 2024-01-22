c1487263c229a08cec3e8388a687ef000929d471
Juan José Rodriguez <zoranbow@gmail.com>
2024-01-21 22:16:50 -0500

    header & User component

--- src/app/(private)/header.tsx
+++ src/app/(private)/header.tsx
@@ -1,5 +1,6 @@
 'use client';
 import useSWR from 'swr';
+import User from '../components/User';
 
 export default function Header (): JSX.Element {
   const { data, error, isLoading } = useSWR('/api/users/profile');
@@ -7,11 +8,14 @@ export default function Header (): JSX.Element {
   if (error) return <div>Failed to load</div>;
   if (isLoading) return <div>Loading...</div>;
 
-  console.log(data);
-
   return (
-    <header>
-      HEADER
+    <header className='flex flex-row w-full p-5 bg-slate-800 rounded-lg my-2 justify-between items-center'>
+      <div>
+        <h1 className='font-mono text-lg'>Strings</h1>
+      </div>
+      <div>
+        <User user={data.data} href='account' />
+      </div>
     </header>
   );
 }
---
+++ src/app/types.ts
@@ -0,0 +1,6 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+interface UserI {
+  id: number;
+  username: string;
+  avatar: string;
+}
