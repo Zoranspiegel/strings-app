e02631da6694784176cf7ae052e9449b559f5315
Juan José Rodriguez <zoranbow@gmail.com>
2024-02-09 17:33:48 -0500

    follow user endpoint

--- src/app/api/follows/route.ts
+++ src/app/api/follows/route.ts
@@ -15,3 +15,25 @@ export async function GET (request: Request): Promise<NextResponse> {
   const response = NextResponse.json(res.rows, { status: 200 });
   return response;
 }
+
+export async function POST (request: Request): Promise<NextResponse> {
+  const resJSON = await request.json();
+  const { userId } = resJSON;
+  const jwtPayload = await getJWTPayload();
+
+  const res = await sql(
+    'select * from public.follows where user_id = $1 and follower_id = $2',
+    [userId, jwtPayload?.sub]
+  );
+
+  if (res.rowCount) {
+    return NextResponse.json({ error: 'User already followed' }, { status: 400 });
+  }
+
+  await sql(
+    'insert into public.follows (user_id, follower_id) values ($1, $2) returning *',
+    [userId, jwtPayload?.sub]
+  );
+  const response = NextResponse.json({ msg: 'User successfully followed' }, { status: 201 });
+  return response;
+}
